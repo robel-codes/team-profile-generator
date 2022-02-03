2ada922c7ae55cf5aca465b64e0fd99b5717a950
Robel <rofikre@yahoo.com>
2022-02-02 23:49:00 -0500

    page template added

--- src/page-template.js
+++ src/page-template.js
@@ -74,3 +74,97 @@ const internHTML = function (intern) {
 }
 
 
+module.exports = (data) => {
+
+    cardArray = [];
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+        //If role is manager generate Managers card 
+        if (role === 'Manager') {
+            const managerCard = managerHTML(employee);
+            cardArray.push(managerCard);
+        }
+
+        //If role is Engineer generate Engineers card 
+        if (role === 'Engineer') {
+            const engineerCard = engineerHTML(employee);
+        
+            cardArray.push(engineerCard);
+        }
+
+        //If role is Intern generate Intern card 
+        if (role === 'Intern') {
+            const internCard = internHTML(employee);
+        
+            cardArray.push(internCard);
+        }
+    }
+
+    const employeeCards = cardArray.join('')
+
+    return pageHTML(employeeCards);
+}
+
+//function to generate page. 
+const pageHTML = function (employeeCards) {
+    return`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <style>
+      header {
+        text-align: center;
+        background-color: red;
+        color: whitesmoke;
+        height: 90px;
+      }
+      span{
+        font-size: 20px;
+      }
+      a{
+        font-size: 20px;
+      }
+    </style>
+</head>
+</head>
+
+
+<body>
+    <body>
+        <header class="red white-text center-align">
+            <h1>Team Profile</h1>           
+        </header>
+
+
+
+    <main>
+        <div class="container">
+            <div class="row justify-content-center" id="team-cards">
+                    <!--Team Information sections-->
+                    ${employeeCards}     
+            </div>
+        </div>
+    </main>
+
+</body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" integrity="sha512-NiWqa2rceHnN3Z5j6mSAvbwwg3tiwVNxiAQaaSMSXnRRDh5C2mk/+sKQRw8qjV1vN4nf8iK2a0b048PnHbyx+Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</html>  
+
+
+`;
+}
+
